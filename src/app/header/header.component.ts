af5eff9cfcc61a57b010d5001d30a197c3c43dbf
Mohamed Naski <mohamed.naski@supcom.tn>
2023-03-25 22:29:33 +0100

    The latest design

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Renderer2, Inject, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  constructor(
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document
+  ) { }
 
+  ngOnInit() {
+    window.addEventListener('scroll', this.scrollFunction, true);
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scrollFunction, true);
+  }
+
+  scrollFunction = () => {
+    const logo = this.document.querySelector(".logo") as HTMLImageElement;
+    if (window.scrollY > 0) {
+      logo.src = "assets/image/EvalQA-scroll.png";
+    } else {
+      logo.src = "assets/image/EvalQA.png";
+    }
+
+    const navbar = this.document.querySelector('.navbar');
+    if (navbar) {
+      if (this.document.body.scrollTop > 80 || this.document.documentElement.scrollTop > 80) {
+        this.renderer.addClass(navbar, 'scrolled-navbar');
+      } else {
+        this.renderer.removeClass(navbar, 'scrolled-navbar');
+      }
+    }
+  };
 }
